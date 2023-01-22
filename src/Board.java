73728a76c2054351ccc7160da0cb1e559a4cc843
Yuval Palatin <yuval@email.com>
2023-01-22 13:45:21 +0200

    finish

--- src/Board.java
+++ src/Board.java
@@ -4,13 +4,15 @@ import java.util.LinkedList;
 public class Board {
    static final int ROW_LEN =6;
    static final int COULMN_HIGHT = 7;
-    public static void printBoard(LinkedList<Index> playerOne, LinkedList<Index> playerTwo){
+    public static char[][] printBoard(LinkedList<Index> playerOne, LinkedList<Index> playerTwo){
+        char[][] matrix = new char[ROW_LEN][COULMN_HIGHT];
         for(int column = COULMN_HIGHT-1; column>=0; column--){
             for(int row=0; row<ROW_LEN;row++){
                 boolean wasFond = false;
                 for(Index in: playerOne){
                     if(in.row == row && in.column == column){
                         wasFond =true;
+                        matrix[row][column] = 'x';
                         System.out.print("|X");
                     }
                 }
@@ -19,6 +21,7 @@ public class Board {
                     for(Index in: playerTwo) {
                         if (in.row == row && in.column == column) {
                             wasFond = true;
+                            matrix[row][column] = 'o';
                             System.out.print("|O");
                         }
                     }
@@ -35,5 +38,6 @@ public class Board {
             }
             System.out.println();
         }
+        return matrix;
     }
 }
--- src/GameLogic.java
+++ src/GameLogic.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 public class GameLogic {
     private final Player[] players;
-    private int[] lowestEmpty;
+    private final int[] lowestEmpty;
     private boolean isFirstPlayerTurn;
     public GameLogic(){
         this.players = new Player[2];
         this.players[0] = new Player(true);
         this.players[1] = new Player(false);
         this.isFirstPlayerTurn = true;
-        this.lowestEmpty = new int[6];
-        for(int i=0; i<6; i++){
+        this.lowestEmpty = new int[Board.ROW_LEN];
+        for(int i=0; i<Board.ROW_LEN; i++){
             this.lowestEmpty[i] =0;
         }
     }
@@ -28,15 +28,73 @@ public class GameLogic {
 
             LinkedList<Index> playerOne = this.players[0].getPlayedMoves();
             LinkedList<Index> playerTwo = this.players[1].getPlayedMoves();
-            Board.printBoard(playerOne,playerTwo);
-            gameEnded = checkWin(playerOne,playerTwo);
+           char[][] mat = Board.printBoard(playerOne,playerTwo);
+
+            if(checkWin(mat)){
+                gameEnded = true;
+                if(this.isFirstPlayerTurn)
+                    System.out.println("Player One is the winner");
+                else
+                    System.out.println("Player Two is the winner");
+            }
+
             this.isFirstPlayerTurn= !this.isFirstPlayerTurn;
         }
     }
 
-    private boolean checkWin(LinkedList<Index> playerOne, LinkedList<Index> playerTwo) {
-        return true;
+    private boolean checkWin(char[][] mat) {
+        for(int row = 0; row<Board.ROW_LEN-3;row++){
+            for(int col=0; col<Board.COULMN_HIGHT;col++)
+            {
+                if(mat[row][col] =='x' || mat[row][col] =='o') {
+                    if (mat[row][col] == mat[row + 1][col] &&
+                            mat[row][col] == mat[row + 2][col] &&
+                            mat[row][col] == mat[row + 3][col]) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        for(int row = 0; row<Board.ROW_LEN;row++){
+            for(int col=0; col<Board.COULMN_HIGHT-3;col++)
+            {
+                if(mat[row][col] =='x' || mat[row][col] =='o') {
+                    if (mat[row][col] == mat[row][col + 1] &&
+                            mat[row][col] == mat[row][col + 2] &&
+                            mat[row][col] == mat[row][col + 3]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        for(int row = 0; row<Board.ROW_LEN-3;row++) {
+            for (int col = 0; col < Board.COULMN_HIGHT - 3; col++) {
+                if (mat[row][col] == 'x' || mat[row][col] == 'o') {
+                    if (mat[row][col] == mat[row + 1][col + 1] &&
+                            mat[row][col] == mat[row + 2][col + 2] &&
+                            mat[row][col] == mat[row + 3][col + 3]) {
+                        return true;
+                    }
+
+                }
+            }
+        }
+        for(int row = 0; row<Board.ROW_LEN-3;row++){
+            for(int col=Board.COULMN_HIGHT-1; col>=3;col--)
+            {
+                if(mat[row][col] =='x' || mat[row][col] =='o') {
+                    if (mat[row][col] == mat[row + 1][col - 1] &&
+                            mat[row][col] == mat[row + 2][col - 2] &&
+                            mat[row][col] == mat[row + 3][col - 3]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
+
 
     private  Player printMessegeToCurrPlayer(){
         Player temp;
